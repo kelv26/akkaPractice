adfc95302df21710faecc953c8b2be8de2ca2f0f
Kelv26 <75353882+kelv26@users.noreply.github.com>
2023-08-07 13:52:19 +0800

    Deleted unnecessary files

--- shopping-order-service/src/main/scala/shopping/order/CborSerializable.scala
+++
@@ -1,7 +0,0 @@
-package shopping.order
-
-/**
- * Marker trait for serialization with Jackson CBOR.
- * Enabled in serialization.conf `akka.actor.serialization-bindings` (via application.conf).
- */
-trait CborSerializable
